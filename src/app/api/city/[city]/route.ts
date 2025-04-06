c7b3c39a762a52ce09a489db4cf6fe9d5f398cf2
Archit Yadav <amky40011@gmail.com>
2025-04-06 23:11:15 +0530

    changed again city route.ts

--- src/app/api/city/[city]/route.ts
+++ src/app/api/city/[city]/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest } from "next/server";
 import { getCityWeatherData } from "@/lib/getCittyWeatherData";
 
-export async function GET(req: NextRequest, { params }: { params: { city: string } }) {
-  const city = params.city;
-  const data = await getCityWeatherData(city);
+// Use `context` as the second argument and destructure from it
+export async function GET(req: NextRequest, context: { params: { city: string } }) {
+  const { city } = context.params;
 
+  const data = await getCityWeatherData(city);
   return Response.json(data);
 }

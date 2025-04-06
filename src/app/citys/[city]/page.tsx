3f0fdb4022bc49af4fa0048d7c5514663f52496e
Archit Yadav <amky40011@gmail.com>
2025-04-06 22:20:08 +0530

    Getweatherdata updated

--- src/app/citys/[city]/page.tsx
+++ src/app/citys/[city]/page.tsx
@@ -1,11 +1,19 @@
-import { getCityWeatherData } from "@/app/api/city/[city]/route";
 import CityPageClient from "./client_page";
 
 export const revalidate = 1800;
 
 export default async function CityPage({ params }: { params: { city: string } }) {
   const city = params.city;
-  const data = await getCityWeatherData(city);
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/city/${city}`, {
+    next: { revalidate: 1800 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch city weather data");
+  }
+
+  const data = await res.json();
 
   return (
     <CityPageClient
